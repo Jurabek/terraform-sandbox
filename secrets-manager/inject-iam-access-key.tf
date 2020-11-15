4f1dcdfd7de97d9f56e8a2b79135e200e13e8eee
jurabek <jurabek.azizkhujaev@gmail.com>
2020-11-15 18:59:08 +0100

    IAM user create and inject to Secrets manager

--- secrets-manager/inject-iam-access-key.tf
+++ secrets-manager/inject-iam-access-key.tf
@@ -1,14 +1,14 @@
 resource "aws_secretsmanager_secret" "dynamodb-user-secrets" {
-  name     = "pnc-oem-user-secret"
+  name     = "dynamodb_user-secret"
 }
 
 resource "aws_secretsmanager_secret_version" "dynamodb-user-secrets" {
   secret_id     = "dynamodb-user-secret"
   secret_string = jsonencode({
-    "AccessKeyId" = "${aws_iam_access_key.pnc-oem-user-access-key.id}"
-    "SecretAccessKey" = "${aws_iam_access_key.pnc-oem-user-access-key.secret}"
+    "AccessKeyId" = "${aws_iam_access_key.dynamodb_user-access-key.id}"
+    "SecretAccessKey" = "${aws_iam_access_key.dynamodb_user-access-key.secret}"
   })
   depends_on = [
-      aws_iam_user.pnc-oem-user
+      aws_iam_user.dynamodb_user
   ]
 }
