1401ad90edd16aaf99c3d24443134b8820beab95
jurabek <jurabek.azizkhujaev@gmail.com>
2020-11-15 19:43:15 +0100

    added DynamoDb full access policy to User

--- iam/creat-iam-user.tf
+++ iam/creat-iam-user.tf
@@ -5,3 +5,8 @@ resource "aws_iam_user" "dynamodb_user" {
 resource "aws_iam_access_key" "dynamodb_user" {
   user    = aws_iam_user.dynamodb_user.name
 }
+
+resource "aws_iam_user_policy_attachment" "dynamodb_user-dyn-db-full-access" {
+  user       = aws_iam_user.dynamodb_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
